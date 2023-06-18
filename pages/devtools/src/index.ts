58f59af7d2a65261abebe6d745192cc9eadfd594
y1j2x34 <y1j2x34@qq.com>
2023-06-18 22:10:56 +0800

    chore: add tailwind entry point

--- pages/devtools/src/index.ts
+++ pages/devtools/src/index.ts
@@ -1,5 +1,6 @@
+import '@vgerbot/browser-extension-common/tailwind.css';
 try {
-    chrome.devtools.panels.create('Dev Tools', 'icon-34.png', 'src/pages/panel/index.html');
+    chrome.devtools.panels.create('Dev Tools', 'icon-34.png', '/devtools/index.html');
 } catch (e) {
     console.error(e);
 }
