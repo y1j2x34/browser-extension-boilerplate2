c387499d1e67742bcbf95a86e76c594e27d63f3f
y1j2x34 <y1j2x34@qq.com>
2023-06-20 22:03:22 +0800

    Refactored project structure and file names, and modified configuration files

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('tailwindcss')({}),
+        require('tailwindcss')(require('./tailwind.config.cjs')),
         require('autoprefixer'),
         require('postcss-preset-env')({
             stage: 0
